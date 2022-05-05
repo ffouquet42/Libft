94a8a3d3fabba29e018d12c34ff30d569c4536cc
ffouquet42 <ffouquet42@gmail.com>
2022-05-05 13:35:15 +0200

    add ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -1,28 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   ft_bzero.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/05 10:49:37 by fllanet           #+#    #+#             */
-/*   Updated: 2022/05/05 13:35:02 by fllanet          ###   ########.fr       */
+/*   Created: 2022/05/05 13:27:58 by fllanet           #+#    #+#             */
+/*   Updated: 2022/05/05 13:31:27 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFT_H
-# define LIBFT_H
-# include <unistd.h>
-# include <string.h>
-# include <stdlib.h>
+#include "libft.h"
 
-void	ft_bzero(void *s, size_t n);
-int		ft_isalnum(int c);
-int		ft_isalpha(int c);
-int		ft_isascii(int c);
-int		ft_isdigit(int c);
-int		ft_isprint(int c);
-void	*ft_memset(void *s, int c, size_t n);
-size_t	ft_strlen(const char *s);
+void	ft_bzero(void *s, size_t n)
+{
+	size_t	i;
+	char	*str;
 
-#endif
+	str = (char *)s;
+	i = 0;
+	while (i < n)
+	{
+		str[i] = '\0';
+		i++;
+	}
+}
