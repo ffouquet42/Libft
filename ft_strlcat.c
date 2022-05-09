4c893aabb2b55f364a8134ab8e86193fd09858a2
ffouquet42 <ffouquet42@gmail.com>
2022-05-09 22:33:09 +0200

    add ft_strlcat.c

---
+++ ft_strlcat.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/09 22:19:19 by fllanet           #+#    #+#             */
+/*   Updated: 2022/05/09 22:32:02 by fllanet          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
+{
+	size_t	dst_size;
+	size_t	src_size;
+	size_t	i;
+
+	src_size = ft_strlen(src);
+	dst_size = ft_strlen(dst);
+	if (size == 0 || dst_size >= size)
+		return (src_size + size);
+	i = 0;
+	while (src[i] && (i + dst_size + 1 < size))
+	{
+		dst[dst_size + i] = src[i];
+		i++;
+	}
+	dst[dst_size + i] = '\0';
+	return (dst_size + src_size);
+}
+
+/*
+#include <stdio.h>
+int main()
+{
+	char *dest = "dest";
+	const char *src = "source";
+	size_t size = 1;
+
+	printf("%zu\n", ft_strlcat(dest, src, size));
+}
+*/
