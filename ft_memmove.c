fbd79e8a1fdd8fb08b19da44d9612ec22afb0777
ffouquet42 <ffouquet42@gmail.com>
2022-05-09 21:04:08 +0200

    not standardized

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,34 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/07 22:35:48 by fllanet           #+#    #+#             */
-/*   Updated: 2022/05/07 22:35:51 by fllanet          ###   ########.fr       */
+/*   Updated: 2022/05/09 21:00:59 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	size_t			i;
+	unsigned char	*dst_tmp;
+	unsigned char	*src_tmp;
+	unsigned char	tmp[n];
+
+	i = 0;
+	if (!dest && !src)
+		return (NULL);
+	dst_tmp = (unsigned char *)dest;
+	src_tmp = (unsigned char *)src;
+	while (i < n)
+	{
+		tmp[i] = src_tmp[i];
+		i++;
+	}
+	i = 0;
+	while (i < n)
+	{
+		dst_tmp[i] = tmp[i];
+		i++;
+	}
+	return (dest);
+}
