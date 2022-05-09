0752eda6560a16e35d65775a4952825e15d2fd2d
ffouquet42 <ffouquet42@gmail.com>
2022-05-09 22:09:56 +0200

    ft_strlcpy.c finished

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -3,10 +3,34 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
+/*   By: fllanet <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/08 21:08:23 by fllanet           #+#    #+#             */
-/*   Updated: 2022/05/08 21:08:25 by fllanet          ###   ########.fr       */
+/*   Updated: 2022/05/09 22:08:55 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (size == 0)
+	{
+		while (src[i])
+			i++;
+		return (i);
+	}
+	while (i < size - 1 && src[i] != '\0')
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	if (i < size)
+		dst[i] = '\0';
+	while (src[i] != '\0')
+		i++;
+	return (i);
+}
