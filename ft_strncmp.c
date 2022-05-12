c2afe92db64178dd25455d2c021f2b7653ff8e44
ffouquet42 <ffouquet42@gmail.com>
2022-05-12 12:08:16 +0200

    fix error founded by second tester

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 01:27:17 by fllanet           #+#    #+#             */
-/*   Updated: 2022/05/10 01:32:44 by fllanet          ###   ########.fr       */
+/*   Updated: 2022/05/12 12:01:56 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	while ((s1[i] || s2[i]) && i < n)
 	{
 		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 		i++;
 	}
 	return (0);
