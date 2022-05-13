dc733daf1fb4890050fd061e4234534a016e5b6f
ffouquet42 <ffouquet42@gmail.com>
2022-05-13 11:25:27 +0200

    file normalization

--- ft_lstclear.c
+++ ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: fllanet <fllanet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/13 09:40:24 by fllanet           #+#    #+#             */
-/*   Updated: 2022/05/13 10:00:12 by fllanet          ###   ########.fr       */
+/*   Updated: 2022/05/13 11:24:43 by fllanet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,12 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	t_list *ptr;
+	t_list	*ptr;
 
 	while (*lst)
 	{
 		ptr = (*lst)->next;
 		ft_lstdelone(*lst, del);
-		(*lst) = ptr;	
+		(*lst) = ptr;
 	}
 }
